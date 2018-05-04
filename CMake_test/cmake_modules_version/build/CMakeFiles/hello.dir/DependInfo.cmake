f2d0d3a2f7504d89841c82a3f0ec6a8fe72c8943
leo <yqnt418@163.com>
2018-05-04 13:57:18 +0800

    update workspace

---
+++ CMake_test/cmake_modules_version/build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/workspace/gitrepo/workspace/CMake_test/cmake_modules_version/main.cpp" "/root/workspace/gitrepo/workspace/CMake_test/cmake_modules_version/build/CMakeFiles/hello.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/workspace/gitrepo/workspace/CMake_test/cmake_modules_version/build/src/CMakeFiles/func.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- CMake_test/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include "hello.h"
-#include <iostream>
-
-using namespace std;
-
-void hello(void){
-    cout << "hello world" << endl;
-}

3ea2e87aee9b6226fa05e8f9e86ad9b4a25588f7
leo <yqnt418@163.com>
2018-05-31 19:46:45 +0800

    update

--- ZTE/zeckendorf/zeckendorf.c
+++ ZTE/zeckendorf/zeckendorf.c
@@ -1,81 +1,38 @@
 #include <stdio.h>
 
-#define N 3
-#define M 4
+#define MAX 20
+#define FLength 6
 
 int main()
 {
-    int i,j;
-    int num=    0;
-    int offset= 0;
+    int i,j,number;
+    int flag;
+    int fibonacci[6] = {1,2,3,5,8,13};
+    char result[7];
+    
 
-    char *str="*.....*.....";
-    int result[N][M]=   {0};
-
-    for(i=0;i<N;i++)
+    for(i=0; i<=MAX; i++)
     {
-        for(j=0;j<M;j++)
-        {
-            //打印字符
-            printf("%c", *(str+i*M+j));
+        flag=0;
+        number=i;
 
-            //当前节点是否为mine(炸弹)
-            if(*(str+i*M+j)=='*')
+        for(j=0; j<FLength; j++)
+        {
+            if(flag==0 && fibonacci[FLength-1-j] <= number)
             {
-                result[i][j]=-1;
-                continue;
+                number -= fibonacci[FLength-1-j];
+                result[j]='1';
+                flag=1;
+            }
+            else
+            {
+                result[j]='0';
+                flag=0;
             }
 
-            //节点周边计算开始
-            num=0;
-            offset = (i-1)*M+j-1;
-            if(i>0 && j>0 && *(str+offset)=='*')
-                num += 1;
-            offset = (i-1)*M+j;
-            if(i>0 && *(str+offset)=='*')
-                num += 1;
-            offset = (i-1)*M+j+1;
-            if(i>0 && j<(M-1) && *(str+offset)=='*')
-                num += 1;
-
-            offset = i*M+j-1;
-            if(j>0 && *(str+offset)=='*')
-                num += 1;
-            offset = i*M+j+1;
-            if(j<(M-1) && *(str+offset)=='*')
-                num += 1;
-
-            offset = (i+1)*M+j-1;
-            if(i<(N-1) && j>0 && *(str+offset)=='*')
-                num += 1;
-            offset = (i+1)*M+j;
-            if(i<(N-1) && *(str+offset)=='*')
-                num += 1;
-            offset = (i+1)*M+j+1;
-            if(i<(N-1) && j<(M-1) && *(str+offset)=='*')
-                num += 1;
-            result[i][j]=num;
-            //节点周边计算结束
-
-
         }
-        printf("\n");
-    }
-
-    printf("-------------\n");
-
-
-    //打印统计结果
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<M;j++)
-        {
-            if(result[i][j]==-1)
-                printf("*");
-            else
-                printf("%d", result[i][j]);
-        }
-        printf("\n");
+        result[6]='\0';
+        printf("%d:\t%s\n", i, result);
     }
 
     return 0;

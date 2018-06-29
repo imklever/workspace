bf466c8511d4ff690464118a2dc1df250a900d66
liutian1 <liutian1@lenovo.com>
2018-06-29 18:59:51 +0800

    update

---
+++ ZTE/max_combination.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+param_inupt=[420,42,423]
+str_tmp=[]
+str_max=""
+str_len=0
+
+for i in param_inupt:
+    str_tmp.append(str(i))
+    print str_tmp
+
+for i in str_tmp:
+    print len(i)

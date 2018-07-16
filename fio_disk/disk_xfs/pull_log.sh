dff2fdc8e73c93f7fbd0d16aaa2bed348a140f4f
liutian1 <liutian1@lenovo.com>
2018-07-16 09:36:07 +0800

    update

--- fio_disk/disk_xfs/pull_log.sh
+++ fio_disk/disk_xfs/pull_log.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-scp -r 10.240.220.37:/root/fio_test/log/* ./log_37
-scp -r 10.240.220.38:/root/fio_test/log/* ./log_38
-scp -r 10.240.220.39:/root/fio_test/log/* ./log_39
+mkdir ./log_37
+cd ./log_37
+scp -r 10.240.220.37:/root/fio_test/log/* ./
+cd ..
+
+mkdir ./log_38
+cd ./log_38
+scp -r 10.240.220.38:/root/fio_test/log/* ./
+cd ..
+
+mkdir ./log_39
+cd ./log_39
+scp -r 10.240.220.39:/root/fio_test/log/* ./
+cd ..

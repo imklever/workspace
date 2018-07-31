f1c4d06563a7c0a628452c51552b96b49944fb4c
liutian1 <liutian1@lenovo.com>
2018-07-31 19:40:00 +0800

    update

---
+++ fio/fio_ceph/map/disk_bare/pull_log.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir log_37
+scp -r 10.240.220.37:/root/fio_test/log/* ./log_37
---
+++ fio/fio_ceph/map/disk_bare/scp_to.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp -p ../install.sh ../check_disk_info.sh ./generate.sh parted.sh 10.240.220.37:/root/fio_test

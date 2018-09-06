c481444fcc6e3b1b74ce7a32b7c0f45486ea4cb8
liutian1 <liutian1@lenovo.com>
2018-09-06 02:12:24 -0400

    update

--- python/time_test.py
+++ python/time_test.py
@@ -3,51 +3,101 @@
 
 import time
 
-#fun = lambda x: print('\033[1;31;43'+x+'\033[0m')
-def printc(x):
-    print
+def printBY(x):
+    #print
     print '\033[30;43m'+x+'\033[0m'
 
-printc('time.timezone:')
+def printFR(x):
+    #print
+    print '\033[31m'+x+'\033[0m'
+
+printBY('转换功能:')
+printFR('string --> timeStamp:')
+printFR('string           --> time.struct_time')
+printFR('time.struct_time --> timeStamp')
+print
+printFR('timeStamp --> string:')
+printFR('timeStamp        --> time.struct_time')
+printFR('time.struct_time --> string')
+
+printBY('time.tzname')
+print type(time.tzname)
+print time.tzname
+
+printBY('time.timezone:')
+print type(time.timezone)
 print time.timezone
 
-printc('time.altzone:')
+printBY('time.altzone:')
+print type(time.altzone)
 print time.altzone
 
-printc('time.daylight:')
+printBY('time.daylight:')
+print type(time.daylight)
 print time.daylight
 
-printc('time.time():')
-print time.time()
+timeStamp       = time.time()
+gmTime          = time.gmtime(timeStamp)
+localTime       = time.localtime(timeStamp)
+ascTime         = time.asctime(localTime)
+cTime           = time.ctime(timeStamp)
+mkTime          = time.mktime(localTime)
+strfTime_gm     = time.strftime('%Y-%m-%d %H:%M:%S', gmTime)
+strfTime_local  = time.strftime('%Y-%m-%d %H:%M:%S', localTime)
+strpTime_gm     = time.strptime(strfTime_gm,'%Y-%m-%d %H:%M:%S')
+strpTime_local  = time.strptime(strfTime_local,'%Y-%m-%d %H:%M:%S')
+strpTime_asc    = time.strptime(strfTime_local,'%Y-%m-%d %H:%M:%S')
 
-#printc ('time.clock():')
+printBY('timeStamp')
+print type(timeStamp)
+print timeStamp
+
+#printBY ('time.clock():')
 #print time.clock()
 
-printc('time.gmtime():')
-print time.gmtime()
+printBY('gmTime <-- timeStamp')
+print type(gmTime)
+print gmTime
 
-printc('time.localtime():')
-print time.localtime()
+printBY('localTime <-- timeStamp')
+print type(localTime)
+print localTime
 
-printc('time.asctime(time.localtime()):')
-print time.asctime(time.localtime())
 
-printc('time.ctime(1536139066.9)')
-print time.ctime(1536139066.9)
+printBY('ascTime <-- localTime')
+print type(ascTime)
+print ascTime
 
-#printc ('time.mktime(time.gmtime())')
-#print time.mktime(time.gmtime())
-printc('time.mktime(time.localtime())')
-print time.mktime(time.localtime())
+printBY('cTime <-- timeStamp')
+print type(time.ctime(time.time()))
+print time.ctime(time.time())
 
-#printc('time.strftime(time.gmtime())')
-#print time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
-printc('time.strftime(time.localtime())')
-print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+printBY('mktime <-- localTime')
+print type(mkTime)
+print mkTime
 
-printc("time.strptime('2018-09-05 05:19:59','%Y-%m-%d %H:%M:%S')")
-print time.strptime('2018-09-05 05:19:59','%Y-%m-%d %H:%M:%S')
+
+printBY('strfTime_gm <-- gmTime')
+print type(strfTime_gm)
+print strfTime_gm
+
+printBY('strfTime_local <-- localTime')
+print type(strfTime_local)
+print strfTime_local
+
+
+printBY('strpTime_gm <-- strfTime_gm')
+print type(strpTime_gm)
+print strpTime_gm
+
+printBY('strpTime_local <-- strfTime_local')
+print type(strpTime_local)
+print strpTime_local
+
+
 #print time.tzset()
 
-printc("time.strptime('time.asctime(time.localtime())','%a %b %d %H:%M:%S %Y')")
-print time.strptime(time.asctime(time.localtime()),'%a %b %d %H:%M:%S %Y')
+printBY("strptime <-- ascTime")
+print type(strpTime_asc)
+print strpTime_asc
+

5abb6b150350da6b3262878323910847f0dd3525
liutian1 <liutian1@lenovo.com>
2018-11-22 15:21:54 +0800

    update

--- template/render.py
+++ template/render.py
@@ -5,11 +5,12 @@ import jinja2
 from jinja2 import Environment,PackageLoader
 
 # 创建一个包加载器对象
-#env = Environment(loader=PackageLoader('python_project','templates'))
-env = Environment(loader=PackageLoader('template'))
+env = Environment(loader=PackageLoader('template_project','templates'))
 
 # 获取一个模板文件
 template = env.get_template('temp')
 
 # 渲染
-template.render(name='haha',age=180)
+content = template.render(name='haha',age=180)
+
+print content
